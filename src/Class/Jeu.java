36712c18c1f4542e563b39a64df6dc9c924bc535
CorentinDodon <corentin.dodon@gmail.com>
2017-10-17 15:13:43 +0200

    ajout gestion pion

--- src/Class/Jeu.java
+++ src/Class/Jeu.java
@@ -1,21 +1,56 @@
 package Class;
 
+import java.util.ArrayList;
+
 public class Jeu {
 	private Joueur[] joueurs;
 	private Plateau plateau;
 	private int nbJoueurs;
+	private int currentPlayer;
 	
 	public Jeu(int nbJoueurs) {
 		this.nbJoueurs = nbJoueurs;
 		this.plateau = new Plateau();
+		this.currentPlayer = 0;
+		this.joueurs = new Joueur[nbJoueurs];
 		switch (this.nbJoueurs) {
 		case 2:
 			this.joueurs[0] = new Joueur(10, Couleur.BLANC);
+			this.joueurs[0].setCoordonnees(new Coordonnees(8,0));
 			this.joueurs[1] = new Joueur(10, Couleur.NOIR);
+			this.joueurs[1].setCoordonnees(new Coordonnees(8,16));
 			break;
 
 		default:
 			break;
 		}
+	}
+
+	public Joueur getCurrentPlayer(){
+		return this.joueurs[currentPlayer];
+	}
+
+	public ArrayList<Coordonnees> getAvailableMove(Joueur j) {
+		return j.getAvailableMove();
+	}
+
+	public boolean moveIfAllow(Joueur j, Coordonnees coord) {
+		if(getCurrentPlayer().equals(j) && getAvailableMove(j).contains(coord)){
+			j.move(coord);
+			this.currentPlayer = (this.currentPlayer + 1) % this.nbJoueurs;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isPlayerHere(Coordonnees coord){
+		boolean result = false;
+		for(int i = 0; i < this.nbJoueurs; i++){
+			if(this.joueurs[i].getCoordonnees().equals(coord)){
+				result = true;
+				break;
+			}
+		}
+		return result;
 	}
 }
--- src/Class/Joueur.java
+++ src/Class/Joueur.java
@@ -2,6 +2,8 @@ package Class;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 
 public class Joueur {
 	private Pion pion;
@@ -26,5 +28,38 @@ public class Joueur {
 		return nbMaxMurs;
 	}
 
+	public void setCoordonnees(Coordonnees coord) {
+		this.pion.setCoordonnees(coord);
+	}
+
+	public Coordonnees getCoordonnees() {
+		return this.pion.getCoordonnees();
+	}
+
+	public ArrayList<Coordonnees> getAvailableMove() {
+		ArrayList<Coordonnees> result = new ArrayList<Coordonnees>();
+		boolean validX, validY;
+		for(int x = 0; x < 17; x++){
+			for(int y = 0; y < 17; y++){
+				int difX, difY;
+				difX = abs(this.getCoordonnees().getX() - x);
+				difY = abs(this.getCoordonnees().getY() - y);
+				validX = (difX == 2) || (difX == 0);
+				validY = (difY == 2) || (difY == 0);
+				if((validX && validY) && (difX + difY == 2)){
+					result.add(new Coordonnees(x,y));
+				}
+			}
+		}
+		return result;
+	}
+
+	public void move(Coordonnees coord) {
+		this.setCoordonnees(coord);
+	}
+
+	public boolean equals(Joueur obj) {
+		return obj.getCouleurs().equals(this.getCouleurs());
+	}
 }
 
