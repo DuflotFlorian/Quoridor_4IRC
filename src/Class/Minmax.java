323eeb17de1dd2ed33826ec1e5daaf049688639f
basile.pracca <basile.pracca>
2018-01-10 15:53:04 +0100

    Setup Minmax

--- src/Class/Minmax.java
+++ src/Class/Minmax.java
@@ -1,5 +1,16 @@
 package Class;
 
 public class Minmax {
+    private static int eval(Game game) {
+
+        return 1;
+    }
+
+    private static String minmax(Game game){
+       int depth = 5;
+       int num_player = game.listPlayer().size();
+       int current_player = 0;
+       return "";
+    }
 
 }
