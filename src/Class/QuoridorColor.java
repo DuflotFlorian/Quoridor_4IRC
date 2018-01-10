38731357b62216b51f3c5b156396204132510c65
ludovic.bancalero <ubiklain@gmail.com>
2018-01-10 16:04:43 +0100

    adaptation de l'ENUM quoridorColor :  > donne un nom français au joueur et est accessible via des accesseurs  > colorise le texte et l'encadrement du joueur courant avec la même couleur que le pion choppé à la pipette

--- src/Class/QuoridorColor.java
+++ src/Class/QuoridorColor.java
@@ -1,5 +1,47 @@
 package Class;
 
+import javax.swing.*;
+import javax.swing.text.html.StyleSheet;
+import java.awt.*;
+
 public enum QuoridorColor {
-    BLUE, RED, GREEN, YELLOW
+
+    BLUE("#0FB1CB", "Joueur BLEU"),
+    RED("#CA2B0F", "Joueur ROUGE"),
+    GREEN("#90D30D", "Joueur VERT"),
+    YELLOW("#DFE219","Joueur JAUNE");
+
+    private Color color_box;
+    private JLabel playerName;
+
+
+    QuoridorColor(String c, String name) {
+        StyleSheet s = new StyleSheet();
+
+        this.color_box  = s.stringToColor(c);
+        this.playerName = new JLabel(name);
+        this.playerName.setForeground(this.color_box);
+        this.playerName.setHorizontalAlignment(JLabel.CENTER);
+    }
+
+
+    public Color getColor_box() {
+        return color_box;
+    }
+
+    public void setColor_box(Color color_box) {
+        this.color_box = color_box;
+    }
+
+    public JLabel getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(JLabel playerName) {
+        this.playerName = playerName;
+    }
 }
+
+
+
+
